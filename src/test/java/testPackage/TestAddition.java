a678aef43fa9ba4e1fae38d4bf079c97a47b2934
ninjasdet <srinarva.n@gmail.com>
2025-01-20 11:51:02 -0500

    addition class check for siri branch

--- src/test/java/testPackage/TestAddition.java
+++ src/test/java/testPackage/TestAddition.java
@@ -4,5 +4,6 @@ public class TestAddition {
 
 	public static void main(String[] args) {
 		System.out.println("i am in addition");
+		System.out.println("checking addition class for siri branch");
 	}
 }
